5aaae68f3ea29db200c8bb166b1eec470713a09b
qbacid <racidk@gmail.com>
2025-10-11 18:05:22 -0500

    initial identity added
    category added to regular quizzes

---
+++ Dentor.Academy.Web/Migrations/20251010220341_AddCategoryToQuiz.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dentor.Academy.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryToQuiz : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "category",
+                table: "quizzes",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "category",
+                table: "quizzes");
+        }
+    }
+}
